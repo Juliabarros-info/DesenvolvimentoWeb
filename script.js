0f31b507ea95f2e288e83d93e3cb9e8cd65fd93a
Juliabarros-info <117217613+Juliabarros-info@users.noreply.github.com>
2023-03-21 07:38:15 -0300

    Add files via upload
    Aula01 - 2B

---
+++ script.js
@@ -0,0 +1,12 @@
+/*     console.log("Hello World") */
+    document.write("Hello World, Manipulando o DOM <br>")
+    /* alert("Hello World com Eventos") */
+    document.write("Nome: Júlia Batista Barros <br>")
+    document.write("Ano: 2° ano <br>")
+    document.write("Curso: Informática <br>")
+    document.write("Localidade: Crateús <br>")
+
+    alert("Nome: Júlia Batista Barros")
+    alert("Ano: 2B")
+    alert("Curso: Informática")
+    alert("Localidade: Crateús")
